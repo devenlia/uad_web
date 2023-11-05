72ff6efde308b980eaa3fe1fbc9d38566883106b
Jannis <j.machowetz@devenlia.com>
2023-11-05 17:08:21 +0100

    Update SVG and error handling in Svelte
    Modified SVG attributes in catty.svg for better handling of rendering dimensions and color. Updated error handling in error.svelte to display custom messages depending on the status code. Also added home page link and catty SVG as part of the error display.

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,14 +1,43 @@
 <script lang="ts">
     import { page } from '$app/stores';
+    import Catty from "$lib/assets/catty.svg?raw";
+    import IconParkOutlineHouse from 'virtual:icons/icon-park-outline/home';
 
-    console.log($page)
+    console.log($page);
+
+    let heading : string;
+    let pageName = $page.params.page?.split("/").pop();
+
+    switch ($page.status) {
+        case 404:
+            heading = "Not found"
+            break;
+        default:
+            heading = `Error: ${$page.status}`
+    }
 </script>
 
-<div class="navbar bg-base-200 rounded-box">
+<header class="navbar bg-base-200 rounded-box">
     <div class="navbar-start">
-        lul
+        <a href="/home" class="btn btn-square text-lg"><IconParkOutlineHouse/></a>
     </div>
     <div class="navbar-center">
-        <p class="text-5xl font-bold">{$page.status}</p>
+        <p class="text-5xl font-bold">{heading}</p>
     </div>
-</div>
+    <div class="navbar-end"></div>
+</header>
+
+{#if ($page.status === 404)}
+    <div class="flex flex-col items-center justify-center mt-20 overflow-y-auto">
+        <h1 class="text-6xl text-center">Oops!</h1>
+        <div class="max-w-md m-5">{@html Catty}</div>
+        <p class="text-center text-3xl">I was unable to locate the page '{pageName}' you were searching for.</p>
+        <p class="text-center text-3xl">Perhaps it was typed incorrectly?</p>
+    </div>
+{:else }
+    <div class="flex flex-col items-center justify-center mt-20 overflow-y-auto">
+        <h1 class="text-6xl text-center">OH NO!</h1>
+        <div class="max-w-md m-5">{@html Catty}</div>
+        <p class="text-center text-3xl">My application ... its broken.</p>
+    </div>
+{/if}
