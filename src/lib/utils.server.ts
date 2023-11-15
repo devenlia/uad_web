2f1cacc89ca7f4e898c2cfe939d849f61198b4b6
Jannis <j.machowetz@devenlia.com>
2023-11-15 21:28:58 +0100

    Replace static import with dynamic import in utils.server.ts
    The static import for the BACKEND_SERVER environment variable has been replaced with a dynamic import from the .env file in utils.server.ts. This change promotes flexibility as the specific BACKEND_SERVER value can now be updated without modifying the source code, reducing potential for errors in server operations.

--- src/lib/utils.server.ts
+++ src/lib/utils.server.ts
@@ -1,11 +1,11 @@
-import { BACKEND_SERVER } from '$env/static/private';
+import { env } from '$env/dynamic/private';
 import { dev } from '$app/environment';
 import type { HttpMethod } from '@sveltejs/kit';
 
 export const getBackendUrl = () => {
 	if (dev) return 'http://localhost:8080';
 	else {
-		let url = BACKEND_SERVER?.toLowerCase() ?? "server";
+		let url = env.BACKEND_SERVER?.toLowerCase() ?? "server";
 		if (!url) throw new Error("Backend server not defined.")
 		if (url.startsWith('https://') || url.startsWith('http://')) {
 			return url.endsWith('/') ? url.slice(0, -1) : url;
