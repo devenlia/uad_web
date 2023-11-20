92ce5bd3430a2c4a632897ba1c660f863d209a89
Jannis <j.machowetz@devenlia.com>
2023-11-20 19:39:33 +0100

    Updated error page layout and replaced Catty image with PlaceholderCat
    The current update includes a refactoring of the error page structure, enhancing the layout by adding a wrapper div for better alignment. We have also replaced the existing asset 'Catty' with 'PlaceholderCat' for the error display, providing a well-rounded user experience and visual consistency across all error pages.

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -3,6 +3,7 @@
 	import { page } from '$app/stores';
 	import Catty from '$lib/assets/catty.svg?raw';
 	import IconParkOutlineHouse from 'virtual:icons/icon-park-outline/home';
+	import { PlaceholderCat } from '$lib/assets';
 
 	console.log($page);
 
@@ -18,27 +19,30 @@
 	}
 </script>
 
-<header class="navbar bg-base-200 rounded-box">
-	<div class="navbar-start">
-		<a href="/home" class="btn btn-square text-lg"><IconParkOutlineHouse /></a>
-	</div>
-	<div class="navbar-center">
-		<p class="text-5xl font-bold">{heading}</p>
-	</div>
-	<div class="navbar-end" />
-</header>
+<div class="w-full">
+	<header class="navbar bg-base-200 rounded-box">
+		<div class="navbar-start">
+			<a href="/home" class="btn btn-square text-lg"><IconParkOutlineHouse /></a>
+		</div>
+		<div class="navbar-center">
+			<p class="text-5xl font-bold">{heading}</p>
+		</div>
+		<div class="navbar-end" />
+	</header>
 
-{#if $page.status === 404}
-	<div class="flex flex-col items-center justify-center mt-20 overflow-y-auto">
-		<h1 class="text-6xl text-center">Oops!</h1>
-		<div class="max-w-md m-5">{@html Catty}</div>
-		<p class="text-center text-3xl">I was unable to locate the page '{pageName}' you were searching for.</p>
-		<p class="text-center text-3xl">Perhaps it was typed incorrectly?</p>
-	</div>
-{:else}
-	<div class="flex flex-col items-center justify-center mt-20 overflow-y-auto">
-		<h1 class="text-6xl text-center">OH NO!</h1>
-		<div class="max-w-md m-5">{@html Catty}</div>
-		<p class="text-center text-3xl">My application ... its broken.</p>
-	</div>
-{/if}
+	{#if $page.status === 404}
+		<div class="flex flex-col items-center justify-center mt-20 overflow-y-auto">
+			<h1 class="text-6xl text-center">Oops!</h1>
+			<div class="max-w-md m-5"><img width="200px" src="{PlaceholderCat}" alt="Placeholder Cat"></div>
+			<p class="text-center text-3xl">I was unable to locate the page '{pageName}' you were searching for.</p>
+			<p class="text-center text-3xl">Perhaps it was typed incorrectly?</p>
+		</div>
+	{:else}
+		<div class="flex flex-col items-center justify-center mt-20 overflow-y-auto">
+			<h1 class="text-6xl text-center">OH NO!</h1>
+			<!--<div class="max-w-md m-5">{@html Catty}</div>-->
+			<div class="max-w-md m-5"><img width="200px" src="{PlaceholderCat}" alt="Placeholder Cat"></div>
+			<p class="text-center text-3xl">My application ... its broken.</p>
+		</div>
+	{/if}
+</div>
